80b64e0ea45e1cc8a5e1c59d1a80028b933d463a
Lucas Matias <lucasmatias2005@gmail.com>
2026-05-08 15:21:29 -0300

    :sparkles: feat: adding Backend baseURL

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/core/router'
 import i18n from '@/core/i18n'
+import axios from 'axios'
 import '@/shared/styles/global.css'
 
 const app = createApp(App)
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 app.use(router)
 app.use(i18n) 
