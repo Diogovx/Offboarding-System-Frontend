e8a16a15fa2fc04b565aa811f2d50cd2de66bf25
Lucas Matias <lucasmatias2005@gmail.com>
2026-05-08 15:24:05 -0300

    :sparkles: feat: centralizing codes related to translations

--- src/core/i18n/index.js
+++ src/core/i18n/index.js
@@ -4,10 +4,12 @@ import { createI18n } from 'vue-i18n';
 import pt from '@/shared/locales/pt-BR.json';
 import en from '@/shared/locales/en-US.json';
 import id from '@/shared/locales/id-ID.json';
-import ar from '@/shared/locales/ar-SA.json'; 
+import ar from '@/shared/locales/ar-SA.json';
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n'; 
 
 const savedLocale = localStorage.getItem('app_lang') || 'pt';
-
+const isLangMenuOpen = ref(false)
 const i18n = createI18n({
   legacy: false, 
   globalInjection: true, 
@@ -21,5 +23,35 @@ const i18n = createI18n({
     ar: ar
   }
 });
+
+const selectLanguage = (code) => {
+  currentLocale.value = code; 
+  switchLanguage();          
+  isLangMenuOpen.value = false; 
+};
+
+export function useLanguage() {
+  const { locale } = useI18n({ useScope: 'global' });
+  
+  const currentLocale = ref(locale.value);
+
+  const switchLanguage = () => {
+    locale.value = currentLocale.value;
+    localStorage.setItem('app_lang', currentLocale.value);
+
+    if (currentLocale.value === 'ar') {
+      document.documentElement.setAttribute('dir', 'rtl');
+      document.documentElement.setAttribute('lang', 'ar');
+    } else {
+      document.documentElement.setAttribute('dir', 'ltr');
+      document.documentElement.setAttribute('lang', currentLocale.value);
+    }
+  };
+
+  return {
+    currentLocale,
+    switchLanguage
+  };
+}
 
 export default i18n;
